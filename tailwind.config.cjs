68fc6e2f0f5c04671731d8f5bd8a9997788a7571
darpserj <sergiupopa999@outlook.com>
2023-08-11 09:56:14 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        offWhite: "#E9E7DD",
+        darkGray: "#282C28",
+        sparkPink: "#F05561",
+        sparkBlue: "#17BDCB",
+        sparkOrange: "#F68D3B",
+        sparkGreen: "#87C75D",
+      },
+    },
+  },
+  plugins: [],
+};
